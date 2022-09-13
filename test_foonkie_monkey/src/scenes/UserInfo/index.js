84ca266cd3ac5df536ec846dbd593fee58dab65f
Juan David Coronado Saldaña <juandacor@outlook.com>
2022-09-12 23:44:07 -0500

    UserInfo UI

--- test_foonkie_monkey/src/scenes/UserInfo/index.js
+++ test_foonkie_monkey/src/scenes/UserInfo/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Image} from 'react-native';
+import SimpleText from 'components/SimpleText';
 import {styles} from './styles';
 
-const UserInfo = () => (
-  <View style={styles.container}>
-    <Text>UserInfo</Text>
-  </View>
-);
+const DUMMY_USER = {
+  id: 3,
+  email: 'emma.wong@reqres.in',
+  first_name: 'Emma',
+  last_name: 'Wong',
+  avatar: 'https://reqres.in/img/faces/3-image.jpg',
+};
+
+const UserInfo = () => {
+  const user = DUMMY_USER;
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.avatar} source={{url: user.avatar}} />
+      <SimpleText
+        style={styles.fullName}
+        text={`${user.first_name} ${user.last_name}`}
+      />
+      <SimpleText text={user.email} />
+    </View>
+  );
+};
 
 export default UserInfo;
